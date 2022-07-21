176aa61586269876a1e827529b57cc31496bc74c
YounessBird <67457600+YounessBird@users.noreply.github.com>
2022-07-21 07:02:25 +0100

    import export model state

--- Models/Extensions.cs
+++ Models/Extensions.cs
@@ -1,18 +1,112 @@
-using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace HotelMS.Extensions
 {
+    public class ModelStateTransferValue
+    {
+        public string Key { get; set; }
+        public string AttemptedValue { get; set; }
+        public object RawValue { get; set; }
+        public ICollection<string> ErrorMessages { get; set; } = new List<string>();
+    }
+    public static class ModelStateHelpers
+    {
+        public static string SerialiseModelState(ModelStateDictionary modelState)
+        {
+            var errorList = modelState
+                .Select(kvp => new ModelStateTransferValue
+                {
+                    Key = kvp.Key,
+                    AttemptedValue = kvp.Value.AttemptedValue,
+                    RawValue = kvp.Value.RawValue,
+                    ErrorMessages = kvp.Value.Errors.Select(err => err.ErrorMessage).ToList(),
+                });
+
+            return JsonConvert.SerializeObject(errorList);
+        }
+
+        public static ModelStateDictionary DeserialiseModelState(string serialisedErrorList)
+        {
+            var errorList = JsonConvert.DeserializeObject<List<ModelStateTransferValue>>(serialisedErrorList);
+            var modelState = new ModelStateDictionary();
+
+            foreach (var item in errorList)
+            {
+                modelState.SetModelValue(item.Key, item.RawValue, item.AttemptedValue);
+                foreach (var error in item.ErrorMessages)
+                {
+                    modelState.AddModelError(item.Key, error);
+                }
+            }
+            return modelState;
+        }
+    }
     public static class SessionExtensions
     {
         public static void Set<T>(this ISession session, string key, T value)
         {
-            session.SetString(key, JsonSerializer.Serialize(value));
+            session.SetString(key, System.Text.Json.JsonSerializer.Serialize(value));
         }
 
         public static T? Get<T>(this ISession session, string key)
         {
             var value = session.GetString(key);
-            return value == null ? default : JsonSerializer.Deserialize<T>(value);
+            return value == null ? default : System.Text.Json.JsonSerializer.Deserialize<T>(value);
+        }
+    }
+
+    public abstract class ModelStateTransfer : ActionFilterAttribute
+    {
+        protected const string Key = nameof(ModelStateTransfer);
+    }
+
+    public class ExportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            //Only export when ModelState is not valid
+            if (!filterContext.ModelState.IsValid)
+            {
+                //Export if we are redirecting
+                if (filterContext.Result is RedirectResult
+                    || filterContext.Result is RedirectToRouteResult
+                    || filterContext.Result is RedirectToActionResult)
+                {
+                    if (filterContext.Controller is Controller controller && filterContext.ModelState != null)
+                    {
+                        var modelState = ModelStateHelpers.SerialiseModelState(filterContext.ModelState);
+                        controller.TempData[Key] = modelState;
+                    }
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
+        }
+    }
+
+    public class ImportModelStateAttribute : ModelStateTransfer
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var controller = filterContext.Controller as Controller;
+
+            if (controller?.TempData[Key] is string serialisedModelState)
+            {
+                if (filterContext.Result is ViewResult)
+                {
+                    var modelState = ModelStateHelpers.DeserialiseModelState(serialisedModelState);
+                    filterContext.ModelState.Merge(modelState);
+                }
+                else
+                {
+                    controller.TempData.Remove(Key);
+                }
+            }
+
+            base.OnActionExecuted(filterContext);
         }
     }
 }
