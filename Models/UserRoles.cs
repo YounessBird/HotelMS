c194f8d69b1a68f09af1355e206e382655f439b2
YounessBird <67457600+YounessBird@users.noreply.github.com>
2022-07-25 09:45:15 +0100

    User Roles Models

---
+++ Models/UserRoles.cs
@@ -0,0 +1,9 @@
+namespace HotelMS.Models
+{
+    public class UserRoles
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string Isselected { get; set; }
+    }
+}
