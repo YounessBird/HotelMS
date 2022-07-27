f2fee8772256f64b4b9ea4d9aa23c9c11e13b0d7
YounessBird <67457600+YounessBird@users.noreply.github.com>
2022-07-27 22:13:26 +0100

    User Model

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HotelMS.Models
+{
+    public class User
+    {
+        public int? Id { get; set; }
+        [Required(ErrorMessage = "Please fill in the Name field")]
+        [MaxLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
+        public string? UName { get; set; }
+        [DataType(DataType.PhoneNumber)]
+        public string? UPhone { get; set; }
+        public string? UGender { get; set; }
+        [Required(ErrorMessage = "Please fill in the email address field")]
+        [EmailAddress(ErrorMessage = "Please Enter a Valid Email Address")]
+        [Display(Name = "Email")]
+        public string? UEmail { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string? UPassword { get; set; }
+    }
+}
